26b25f0362a9c4f32f3b25fac7f59fa208cdeed0
oskar <oskarpadu31@gmail.com>
2025-11-10 14:35:12 +0200

    Create project basic files

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(3011, () => {
+    console.log('Server is running on port 3011');
+});
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from 'express';
+import todoRoutes from './routes/todos.js';
+
+const app = express();
+
+app.use('/todos', todoRoutes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: error.message });
+});
+
+app.listen(3011, () => {
+    console.log('Server is running on port 3011');
+});
